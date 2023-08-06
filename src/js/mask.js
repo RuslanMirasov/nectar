332231cd4fb2382681b4a6ecb0e86dae7abf99db
RuslanMirasov <info@mirasov.dev>
2023-08-06 16:26:09 +0200

    inner pages

--- src/js/mask.js
+++ src/js/mask.js
@@ -2,7 +2,16 @@ import Inputmask from 'inputmask';
 
 const phones = document.querySelectorAll('.input--tel');
 const ruMask = new Inputmask('+7 (999) 999-99-99');
+const etMask = new Inputmask('+372 9999 9999');
+let currentMask = ruMask;
+const pageUrlText = window.location.href;
+const checkEn = pageUrlText.includes('en/');
+const checkEt = pageUrlText.includes('et/');
+
+if (checkEn == true || checkEt == true) {
+  currentMask = etMask;
+}
 
 phones.forEach(phone => {
-  ruMask.mask(phone);
+  currentMask.mask(phone);
 });
